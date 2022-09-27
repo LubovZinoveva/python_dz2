50eccabadd5d8e53dc8da5f0d1b252b8fe90abae
«Lubov <pitisyka@mail.ru>
2022-09-28 00:17:47 +0300

    task 4 fixed

--- Task04.py
+++ Task04.py
@@ -3,24 +3,24 @@
 
 def get_point_coordinates(n):
     point = []
-    for i in range(1, n + 1):
-        point.append(int(input(i + ' = ')))
+    for i in range(n):
+        point.append(int(input(f'{i + 1} = ')))
     return point
     
 def distance(D, point1, point2):
-    result = (point2[i] - point1[i])**2 
+    result = (point2[0] - point1[0])**2 
     for i in range(1,D):
         result += (point2[i] - point1[i])**2 
     return result**0.5
     
 try:
     N = int(input('N = '))
-    print(f'Введите {N} координат первой точки')
+    print(f'Введите {N} координат(ы) первой точки')
     X = get_point_coordinates(N)
     print(f'Введите {N} координат второй точки')
     Y = get_point_coordinates(N)
     my_distance = distance(N, X, Y)
-    print(my_distance)
+    print(f'Расстояние между точками = {round(my_distance, 2)}')
 except:
-    print('Задайте координаты целыми числами')
+    print('Задайте координаты целыми числами > 0')
 
